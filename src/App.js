15881ebf58ea716433a3404fc80f6879a8f22a1a
Kevin Willian <kevinwillian084@gmail.com>
2021-07-22 17:18:10 -0300

    Add exercise 4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path="/" component={ Home } />
+        <Route exact path="/" component={ Home } />
         <Route path="/about" component={ About } />
       </BrowserRouter>
     );

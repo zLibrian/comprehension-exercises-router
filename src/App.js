58ef0ffce4d94a0a4f8446901850da32e2e973a6
Kevin Willian <kevinwillian084@gmail.com>
2021-07-23 17:34:46 -0300

    Add exercise 10

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
@@ -9,9 +9,11 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Header />
-        <Route exact path="/" component={ Home } />
-        <Route path="/about" component={ About } />
-        <Route path="/users/:id" render={(props) => <Users {...props} greetingsMessage="Good Morning"/> } />
+        <Switch>
+          <Route path="/users/:id" render={(props) => <Users {...props} greetingsMessage="Good Morning"/> } />
+          <Route path="/about" component={ About } />
+          <Route exact path="/" component={ Home } />
+        </Switch>
         <Link to="/" >Home</Link>
         <Link to="/about" >About</Link>
         <Link to="/users/10" >Users</Link>
